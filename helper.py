3f8e3145a4b0dc101a5f5bb9a790e4d3006cca30
Jose Vitor <jose.vitor.sem.c@gmail.com>
2025-06-14 10:56:02 -0300

    Heston able to simulate paths

--- helper.py
+++ helper.py
@@ -7,6 +7,45 @@ from arch import arch_model
 class HestonStock:
     def __init__(self):
         self.vt_parameters = None
+        self.st_parameters = 0.98
+
+    def calc_vol(self, vt_last, dt, t, z):
+        kappa, theta, sigma, lambda_ = self.vt_parameters
+        kappa_star = kappa + lambda_
+        theta_star = kappa * theta / (kappa_star)
+
+        drift = kappa_star * (theta_star - vt_last) * dt
+        diffusion = sigma * np.sqrt(vt_last * t) * z
+        return max(drift + diffusion, 0)
+
+    def calc_price(self, vt_last, st_last, dt, t, z):
+        mu = self.st_parameters
+
+        drift = mu * st_last * dt
+        diffusion = np.sqrt(vt_last * t) * st_last * z
+        return drift + diffusion
+
+    def simulate(self, v0, s0, num_days, num_paths):
+        dt = day / num_days
+
+        vol_simulations = np.zeros((num_paths, num_days))
+        price_simulations = np.zeros((num_paths, num_days))
+        vol_simulations[:, 0] = v0
+        price_simulations[:, 0] = s0
+
+        for path in range(num_paths):
+            for day in range(num_days - 1):
+                vol_z, price_z = multivariate_normal.rvs(size=2)
+
+                vt_last = vol_simulations[path, day]
+                vt_new = self.calc_vol(vt_last, dt, dt * day, vol_z)
+                vol_simulations[path, day + 1] = vt_new
+
+                st_last = price_simulations[path, day]
+                st_new = self.calc_price(vt_last, st_last, dt, dt * day, price_z)
+                price_simulations[path, day + 1] = st_new
+
+        return vol_simulations, price_simulations
 
     def _optimize_par_vol_path(self, vt, par0) -> None:
         z = np.random.normal(size=len(vt))
