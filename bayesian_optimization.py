71cb3b2f5bdc8e4d2546f1d0acf0e51aa651a70d
Jose Vitor <jose.vitor.sem.c@gmail.com>
2025-06-15 19:08:58 -0300

    Optimization algorithm structure

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -1,23 +1,45 @@
 import numpy as np
+import pandas as pd
 from scipy import stats
 
 
 dt = 1
 
+
+def particle_filtering():
+    pass
+
+
+def sample_parameters():
+    pass
+
+
 # Gruszka and Szwabiński, 2022
-mu_prior_eta = 1.00125
-sigma_prior_eta = 0.001
-precision_prior_vol = np.array([[10, 0], [0, 5]])
-mu_prior_vol = np.array([35e-6, 0.988])
-a_prior_sigma = 149
-b_prior_sigma = 0.025
-mu_prior_psi = -0.45
-sigma_prior_psi = 0.3
-a_prior_omega = 1.03
-b_prior_omgea = 0.05
-lambda_prior_jump = 0.15
-mu_prior_jump = -0.96
-sigma_prior_jump = 0.3
+def estimate_heston(S: pd.Series, dt, ns, N):
+    n = len(S) - 1
+    mu_prior_eta = 1.00125
+    sigma_prior_eta = 0.001
+    precision_prior_vol = np.array([[10, 0], [0, 5]])
+    mu_prior_vol = np.array([35e-6, 0.988])
+    a_prior_sigma = 149
+    b_prior_sigma = 0.025
+    mu_prior_psi = -0.45
+    sigma_prior_psi = 0.3
+    a_prior_omega = 1.03
+    b_prior_omgea = 0.05
+    # lambda_prior_jump = 0.15
+    # mu_prior_jump = -0.96
+    # sigma_prior_jump = 0.3
+
+    R = S[1:] / S[:-1]
+
+    for i in ns:  # MCMC
+        for k in range(1, n - 1):  # Particle Filtering
+            for j in range(1, N):
+                particle_filtering()
+        sample_parameters()
+
+    return  # parameter estimates
 
 
 def eta_to_mu(eta):
