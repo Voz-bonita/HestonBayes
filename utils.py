d01f5668d97843a6ca441eb0b1b4c03bca3bf2f1
Jose Vitor <jose.vitor.sem.c@gmail.com>
2025-06-22 08:38:10 -0300

    Format for BRL

--- utils.py
+++ utils.py
@@ -1,14 +1,22 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+
+
+def brl_formatter(value, tick_number):
+    return f"R${int(value):,}".replace(",", ".")
 
 
 def plot_wealth(dates, values, labels, title, path):
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    formatter = ticker.FuncFormatter(brl_formatter)
+    ax.yaxis.set_major_formatter(formatter)
     for array, label in zip(values, labels):
-        plt.plot(dates, array, label=label, marker="o")
+        ax.plot(dates, array, label=label, marker="o")
 
     # Formatting the date axis
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+    fig.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
     plt.gcf().autofmt_xdate()  # Auto-rotate date labels
 
     # Add labels and title
